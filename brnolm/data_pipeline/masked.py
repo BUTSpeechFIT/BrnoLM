7aedae382ca0d1e28cd4912ec6e0e5cadc573740
Benes Karel <ibenes@fit.vutbr.cz>
2020-01-29 14:16:42 +0100

    Make a fixed implementation for 1 sentence of two words

--- brnolm/data_pipeline/masked.py
+++ brnolm/data_pipeline/masked.py
@@ -1,4 +1,5 @@
 from typing import List
+import torch
 
 
 def masked_tensor_from_sentences(sentences: List[List[int]]):
@@ -6,3 +7,13 @@ def masked_tensor_from_sentences(sentences: List[List[int]]):
         sentences[0][0]
     except TypeError:
         raise ValueError("masked_tensor_from_sentences() consumes List of Lists (batch X time)")
+
+    input = torch.zeros((1, 1), dtype=torch.int32)
+    target = torch.zeros((1, 1), dtype=torch.int32)
+    mask = torch.zeros((1, 1), dtype=torch.int32)
+
+    input[0, 0] = sentences[0][0]
+    target[0, 0] = sentences[0][1]
+    mask[0, 0] = 1
+
+    return input, target, mask
--- test/test_masked.py
+++ test/test_masked.py
@@ -1,4 +1,5 @@
 from .common import TestCase
+from torch import tensor
 
 
 from brnolm.data_pipeline.masked import masked_tensor_from_sentences
@@ -10,3 +11,20 @@ class MaskedDataCreationTests(TestCase):
 
     def test_requires_batch(self):
         self.assertRaises(ValueError, masked_tensor_from_sentences, [0])
+
+    def test_single_sentence_single_step(self):
+        x, t, m = masked_tensor_from_sentences([[0, 1]])
+
+        e_input = tensor([
+            [0],
+        ])
+        e_target = tensor([
+            [1],
+        ])
+        e_mask = tensor([
+            [1],
+        ])
+
+        self.assertEqual(x, e_input)
+        self.assertEqual(t, e_target)
+        self.assertEqual(m, e_mask)
