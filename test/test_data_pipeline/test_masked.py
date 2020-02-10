a249790fbcfd7e7bb69c87056007ddf221009c7b
Benes Karel <ibenes@fit.vutbr.cz>
2020-02-10 12:40:11 +0100

    Ensure that a mix of single- and multi-token sentences can be handled

--- test/test_data_pipeline/test_masked.py
+++ test/test_data_pipeline/test_masked.py
@@ -176,3 +176,27 @@ class MaskedDataCreationTests(TestCase):
         self.assertEqual(x, e_input)
         self.assertEqual(t, e_target)
         self.assertEqual(m, e_mask)
+
+    def test_single_token_multi_token(self):
+        sentences = [
+            [0],
+            [1, 2],
+        ]
+        x, t, m = masked_tensor_from_sentences(sentences, target_all=True)
+
+        e_input = tensor([
+            [0],
+            [1],
+        ])
+        e_target = tensor([
+            [0, 0],
+            [1, 2],
+        ])
+        e_mask = tensor([
+            [1, 0],
+            [1, 1],
+        ])
+
+        self.assertEqual(x, e_input)
+        self.assertEqual(t, e_target)
+        self.assertEqual(m, e_mask)
