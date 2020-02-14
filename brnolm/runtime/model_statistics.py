b9149b48917841cedc696ba74a111b6486779d6f
Benes Karel <ibenes@fit.vutbr.cz>
2020-02-14 10:24:14 +0100

    Report number of trainable parameters

--- brnolm/runtime/model_statistics.py
+++ brnolm/runtime/model_statistics.py
@@ -5,7 +5,11 @@ class ModelStatistics:
     def total_nb_params(self):
         return sum(p.numel() for p in self.model.parameters())
 
+    def nb_trainable_params(self):
+        return sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+
     def __str__(self):
         torch_desc = f'{self.model}\n'
         nb_params_desc = f'Total number of parameters: {self.total_nb_params()/1000000:.2f}M\n'
-        return torch_desc + nb_params_desc
+        nb_trainable_desc = f'Number of trainable parameters: {self.nb_trainable_params()/1000000:.2f}M\n'
+        return torch_desc + nb_params_desc + nb_trainable_desc
