c88763b97bbcdd2babc2dafd4874050d68e6b997
Benes Karel <ibenes@fit.vutbr.cz>
2020-02-26 13:58:24 +0100

    Use scaled_int_str() when printing out model size

--- brnolm/runtime/model_statistics.py
+++ brnolm/runtime/model_statistics.py
@@ -18,7 +18,7 @@ class ModelStatistics:
         return sum(p.numel() for p in self.model.parameters() if p.requires_grad)
 
     def trainable_params_breakup(self):
-        per_param_desc = (f'{name} {p.numel()}\n' for name, p in self.model.named_parameters() if p.requires_grad)
+        per_param_desc = (f'{name} {scaled_int_str(p.numel())}\n' for name, p in self.model.named_parameters() if p.requires_grad)
         return ''.join(per_param_desc)
 
     def nb_nontrainable_params(self):
@@ -26,7 +26,7 @@ class ModelStatistics:
 
     def __str__(self):
         torch_desc = f'{self.model}\n'
-        nb_params_desc = f'Total number of parameters: {self.total_nb_params()/1000000:.2f}M\n'
-        nb_trainable_desc = f'Number of trainable parameters: {self.nb_trainable_params()/1000000:.2f}M\n'
-        nb_nontrainable_desc = f'Number of nontrainable parameters: {self.nb_nontrainable_params()/1000000:.2f}M\n'
+        nb_params_desc = f'Total number of parameters: {scaled_int_str(self.total_nb_params())}\n'
+        nb_trainable_desc = f'Number of trainable parameters: {scaled_int_str(self.nb_trainable_params())}\n'
+        nb_nontrainable_desc = f'Number of nontrainable parameters: {scaled_int_str(self.nb_nontrainable_params())}\n'
         return torch_desc + nb_params_desc + nb_trainable_desc + nb_nontrainable_desc + self.trainable_params_breakup()
