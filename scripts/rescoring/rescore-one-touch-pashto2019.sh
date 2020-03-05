beb1b23042a185ad11df8302d523b1f2fef09164
Benes Karel <ibenes@fit.vutbr.cz>
2020-03-05 13:02:31 +0100

    update/fix rescoring scripts

--- scripts/rescoring/rescore-one-touch-pashto2019.sh
+++ scripts/rescoring/rescore-one-touch-pashto2019.sh
@@ -1,63 +1,65 @@
 work_dir=$1
 n_jobs=$2
 
-pythlm_model=/mnt/matylda5/ibenes/projects/wsj-rescoring/new-lm.lm
+pythlm_model=/mnt/matylda5/ibenes/projects/opensat-2019/pashto/finetune_lr-1.0
 
-kaldi_decode_dir=/mnt/matylda5/ibenes/kaldi/egs/wsj/s5/exp/tri4b/decode_bd_tgpr_dev93
-kaldi_wordlist=/mnt/matylda5/ibenes/kaldi/egs/wsj/s5/data/lang_test_bd_tgpr/words.txt
-kaldi_unk='<UNK>'
-optimal_lmwt=17.0
-kaldi_eg_dir=/mnt/matylda5/ibenes/kaldi/egs/wsj/s5
+kaldi_decode_dir=/mnt/matylda3/karafiat/OpenSAT/2019/Babel_Pashto.v1/exp/chain_MultRDTv1_short/tdnn_lowrank_unconstrained_sp_nleaves2000_new-swbd-cnn/decode_build_dev.seg1_graph_but-web
+kaldi_wordlist=/mnt/matylda3/karafiat/OpenSAT/2019/Babel_Pashto.v1/data/lang_test_but-web/words.txt
+kaldi_unk='<unk>'
+optimal_lmwt=8
+kaldi_eg_dir=/mnt/matylda3/karafiat/OpenSAT/2019/Babel_Pashto.v1
 
-lmws="15.5 16.5 17.5 18.5 19.5"
-interpolations="0.0 0.05 0.10 0.15 0.20 0.25 0.30 0.35 0.40"
+# lmws="7.0 7.5 8.0 8.5 9.0"
+lmws="9.5 10.0 10.5"
+interpolations="0.0 0.05 0.10 0.15 0.20"
 
-data_dir=data/test_dev93 #  contains reference transcription
-graph_dir=exp/tri4b/graph_bd_tgpr #  who knows what for...
-old_lm="fstproject --project_output=true $kaldi_eg_dir/data/lang_test_bd_tgpr/G.fst |"
+# data_dir=$kaldi_eg_dir/data/build_dev.seg1  # contains reference transcription
+data_dir=$kaldi_eg_dir/data-hires/build_dev.seg1  # contains reference transcription
+graph_dir=$kaldi_eg_dir/exp/chain_MultRDTv1_short/tree_lowrank_unconstrained_sp_nleaves2000/graph_but-web  # who knows what for...
+old_lm="fstproject --project_output=true $kaldi_eg_dir/data/lang_test_but-web/G.fst |"
 
 mkdir -p $work_dir
 
-for ii in $(seq 1 $n_jobs)
-do 
-    lattice-to-nbest \
-            --acoustic-scale=$(echo 1/$optimal_lmwt | bc -l) \
-            --n=100 \
-            "ark:gunzip -c $kaldi_decode_dir/lat.$ii.gz |" ark:- |\
-        tee $work_dir/latt.$ii.nbest |\
-        nbest-to-linear \
-            ark:- \
-            ark,t:$work_dir/$ii.ali \
-            ark,t:$work_dir/$ii.words \
-            ark,t:$work_dir/$ii.hclgscore \
-            ark,t:$work_dir/$ii.acscore || break
+# for ii in $(seq 1 $n_jobs)
+# do 
+#     lattice-to-nbest \
+#             --acoustic-scale=$(echo 1/$optimal_lmwt | bc -l) \
+#             --n=100 \
+#             "ark:gunzip -c $kaldi_decode_dir/lat.$ii.gz |" ark:- |\
+#         tee $work_dir/latt.$ii.nbest |\
+#         nbest-to-linear \
+#             ark:- \
+#             ark,t:$work_dir/$ii.ali \
+#             ark,t:$work_dir/$ii.words \
+#             ark,t:$work_dir/$ii.hclgscore \
+#             ark,t:$work_dir/$ii.acscore || exit 1
+#
+#     lattice-lmrescore \
+#         --lm-scale=-1.0 \
+#         "ark:$work_dir/latt.$ii.nbest" \
+#         "$old_lm" \
+#         ark:- |\
+#         nbest-to-linear \
+#             ark:- \
+#             ark:/dev/null \
+#             ark:/dev/null \
+#             ark,t:$work_dir/$ii.hclscore \
+#             ark:/dev/null || exit 1
+#
+#     scripts/rescoring/score-combiner.py \
+#         $work_dir/$ii.hclgscore 1.0 \
+#         $work_dir/$ii.hclscore -1.0 > $work_dir/$ii.gscore
+# done
 
-    lattice-lmrescore \
-        --lm-scale=-1.0 \
-        "ark:$work_dir/latt.$ii.nbest" \
-        "$old_lm" \
-        ark:- |\
-        nbest-to-linear \
-            ark:- \
-            ark:/dev/null \
-            ark:/dev/null \
-            ark,t:$work_dir/$ii.hclscore \
-            ark:/dev/null || break
-
-    scripts/rescoring/score-combiner.py \
-        $work_dir/$ii.hclgscore 1.0 \
-        $work_dir/$ii.hclscore -1.0 > $work_dir/$ii.gscore
-done
-
-for ii in $(seq 1 $n_jobs) 
-do 
-    scripts/rescoring/rescore-kaldi-latt.py \
-        --latt-vocab=$kaldi_wordlist \
-        --latt-unk=$kaldi_unk \
-        --model-from=$pythlm_model \
-        $work_dir/$ii.words \
-        $work_dir/$ii.rnnlm-scores 
-done
+# for ii in $(seq 1 $n_jobs) 
+# do 
+#     scripts/rescoring/rescore-kaldi-latt.py \
+#         --latt-vocab=$kaldi_wordlist \
+#         --latt-unk=$kaldi_unk \
+#         --model-from=$pythlm_model \
+#         $work_dir/$ii.words \
+#         $work_dir/$ii.rnnlm-scores 
+# done
 
 for lm_w in $lmws
 do
@@ -70,12 +72,26 @@ do
 done
 
 cd $kaldi_eg_dir
-. path.sh
+echo PWD: $PWD
 
-for pickdir in $work_dir/pick-1.0-* 
+export KALDI_ROOT=/mnt/matylda3/karafiat/BABEL/GIT/Kaldi.cur
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
+
+export LC_ALL=C
+export CUDA_CACHE_DISABLE=1
+
+
+# for pickdir in $work_dir/pick-orig-lattices-like-martas
+# for pickdir in $work_dir/pick-orig-lattices 
+for pickdir in  $work_dir/pick-1.0-* 
 do 
-    local/score.sh --min_lmwt 1 --max_lmwt 1 $data_dir $graph_dir $pickdir
+    echo $n_jobs > $pickdir/num_jobs
+    local/score.sh --min_lmwt 8 --max_lmwt 8 $data_dir $graph_dir $pickdir
 done
 
-cat $work_dir/pick-1.0-*/scoring_kaldi/best_wer | grep -v 'nan' | sort -k2 -n > $work_dir/all_wers
+# cat $work_dir/pick-1.0-*/scoring_kaldi/best_wer | grep -v 'nan' | sort -k2 -n > $work_dir/all_wers
+grep 'Error' $work_dir/pick-1.0-8-*/scoring_8/ctm_filt_stm.dtl > $work_dir/all_wers
 
