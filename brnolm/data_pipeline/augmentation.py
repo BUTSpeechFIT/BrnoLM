b072dcab95861317e37b7052c7c55b8141f458a9
Benes Karel <ibenes@fit.vutbr.cz>
2020-04-23 19:33:49 +0200

    Allow rudimentary specification of corruption range

--- brnolm/data_pipeline/augmentation.py
+++ brnolm/data_pipeline/augmentation.py
@@ -2,13 +2,14 @@ import torch
 
 
 class Corruptor:
-    def __init__(self, source, rate):
+    def __init__(self, source, rate, replacements_range):
         self.source = source
         self.rate = rate
+        self.replacement_range = replacements_range
 
     def __iter__(self):
         for X, targets in self.source:
-            replacements = torch.randint(0, 1000, X.shape, device=X.device, dtype=X.dtype)
+            replacements = torch.randint(0, self.replacement_range, X.shape, device=X.device, dtype=X.dtype)
             mask = torch.full(X.shape, self.rate, device=X.device)
             mask = torch.bernoulli(mask).long()
 
