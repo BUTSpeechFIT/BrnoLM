35fe57fc387128b7cef16316bf233c6f55591ba9
Benes Karel <ibenes@fit.vutbr.cz>
2020-04-27 15:01:33 +0200

    Separate substitution from stream providing

--- brnolm/data_pipeline/augmentation.py
+++ brnolm/data_pipeline/augmentation.py
@@ -1,16 +1,24 @@
 import torch
 
 
-class Corruptor:
-    def __init__(self, source, rate, replacements_range):
-        self.source = source
+class Substitutor:
+    def __init__(self, rate, replacements_range):
         self.rate = rate
         self.replacement_range = replacements_range
 
+    def __call__(self, X, targets):
+        replacements = torch.randint(0, self.replacement_range, X.shape, device=X.device, dtype=X.dtype)
+        mask = torch.full(X.shape, self.rate, device=X.device)
+        mask = torch.bernoulli(mask).long()
+
+        return X * (1-mask) + replacements * mask, targets
+
+
+class Corruptor:
+    def __init__(self, source, rate, replacements_range):
+        self.source = source
+        self.substitutor = Substitutor(rate, replacements_range)
+
     def __iter__(self):
         for X, targets in self.source:
-            replacements = torch.randint(0, self.replacement_range, X.shape, device=X.device, dtype=X.dtype)
-            mask = torch.full(X.shape, self.rate, device=X.device)
-            mask = torch.bernoulli(mask).long()
-
-            yield X * (1-mask) + replacements * mask, targets
+            yield self.substitutor(X, targets)
