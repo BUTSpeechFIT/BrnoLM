d4a832df8c5740c86b173d3c9f149c2fa3fef0a6
Benes Karel <ibenes@fit.vutbr.cz>
2020-09-07 09:35:06 +0200

    Upgrade umask setting to an exception-safe resetter

--- brnolm/runtime/safe_gpu.py
+++ brnolm/runtime/safe_gpu.py
@@ -31,28 +31,40 @@ class SafeLocker:
         fcntl.lockf(self._fd, fcntl.LOCK_UN)
 
 
+class SafeUmask:
+    def __init__(self, working_mask):
+        self.old_mask = None
+        self.working_mask = working_mask
+
+    def __enter__(self):
+        self.old_mask = os.umask(self.working_mask)
+
+    def __exit__(self, type, value, traceback):
+        os.umask(self.old_mask)
+
+
 class GPUOwner:
     def __init__(self, placeholder_fn=pytorch_placeholder, logger=None, debug_sleep=0.0):
         if logger is None:
             logger = logging
 
-        os.umask(0)  # do not mask any permission out by default
-        with open(os.open(LOCK_FILENAME, os.O_CREAT | os.O_WRONLY, 0o666), 'w') as f:
-            logger.info(f'acquiring lock')
+        with SafeUmask(0):  # do not mask any permission out by default
+            with open(os.open(LOCK_FILENAME, os.O_CREAT | os.O_WRONLY, 0o666), 'w') as f:
+                logger.info(f'acquiring lock')
 
-            with SafeLocker(f):
-                logger.info(f'lock acquired')
-                setup_cuda_visible_devices()
+                with SafeLocker(f):
+                    logger.info(f'lock acquired')
+                    setup_cuda_visible_devices()
 
-                if not os.environ['CUDA_VISIBLE_DEVICES']:
-                    raise RuntimeError("No free GPUs found. Someone didn't properly declare their gpu resource?")
+                    if not os.environ['CUDA_VISIBLE_DEVICES']:
+                        raise RuntimeError("No free GPUs found. Someone didn't properly declare their gpu resource?")
 
-                logger.info(f"Got CUDA_VISIBLE_DEVICES={os.environ['CUDA_VISIBLE_DEVICES']}")
-                time.sleep(debug_sleep)
+                    logger.info(f"Got CUDA_VISIBLE_DEVICES={os.environ['CUDA_VISIBLE_DEVICES']}")
+                    time.sleep(debug_sleep)
 
-                try:
-                    self.placeholder = placeholder_fn()
-                except RuntimeError:
-                    logger.error(f'Failed to acquire placeholder, truly marvellous')
-                    raise
-            logger.info(f'lock released')
+                    try:
+                        self.placeholder = placeholder_fn()
+                    except RuntimeError:
+                        logger.error(f'Failed to acquire placeholder, truly marvellous')
+                        raise
+                logger.info(f'lock released')
