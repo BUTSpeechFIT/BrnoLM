bb1f2de41f71d4c0ad6c30983c467f237a72a10a
Benes Karel <ibenes@fit.vutbr.cz>
2020-09-30 08:25:16 +0200

    Add support for character evaluation

--- scripts/eval/eval.py
+++ scripts/eval/eval.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch RNN/LSTM Language Model')
     parser.add_argument('--data', type=str, required=True,
                         help='location of the data corpus')
-    parser.add_argument('--shuffle-lines', action='store_true',
-                        help='shuffle lines before every epoch')
+    parser.add_argument('--characters', action='store_true',
+                        help='Treat the file as containing character tokens')
 
     parser.add_argument('--batch-size', type=int, default=20, metavar='N',
                         help='batch size')
@@ -38,7 +38,13 @@ if __name__ == '__main__':
     lm = torch.load(args.load, map_location=device)
     print(lm)
 
-    evaluator = EnblockEvaluator(lm, args.data, args.batch_size, args.target_seq_len)
+    evaluator = EnblockEvaluator(
+        lm,
+        args.data,
+        args.batch_size,
+        args.target_seq_len,
+        tokenize_regime='chars' if args.characters else 'words',
+    )
     eval_report = evaluator.evaluate()
 
     print('total loss {:.1f} | per token loss {:5.2f} | ppl {:8.2f}'.format(eval_report.total_loss, eval_report.loss_per_token, math.exp(eval_report.loss_per_token)))
