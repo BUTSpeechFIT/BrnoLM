2cc1dd6acbdb97887b42d3f59e24df82e5760ef8
Benes Karel <ibenes@fit.vutbr.cz>
2020-12-02 10:04:26 +0100

    Make train-flat use safe_gpu

--- scripts/train/train-flat.py
+++ scripts/train/train-flat.py
@@ -5,6 +5,8 @@ import logging
 import math
 import torch
 
+from safe_gpu.safe_gpu import GPUOwner
+
 from brnolm.data_pipeline.reading import tokens_from_fn
 from brnolm.data_pipeline.multistream import batchify
 from brnolm.data_pipeline.temporal_splitting import TemporalSplits
@@ -18,47 +20,7 @@ from brnolm.runtime.loggers import ProgressLogger
 from brnolm.runtime.reporting import ValidationWatcher
 
 
-def main():
-    logging.basicConfig(level=logging.INFO, format='[%(levelname)s::%(name)s] %(message)s')
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--train', type=str, required=True,
-                        help='location of the train corpus')
-    parser.add_argument('--valid', type=str, required=True,
-                        help='location of the valid corpus')
-    parser.add_argument('--characters', action='store_true',
-                        help='work on character level, whitespace is significant')
-    parser.add_argument('--shuffle-lines', action='store_true',
-                        help='shuffle lines before every epoch')
-
-    parser.add_argument('--batch-size', type=int, default=20, metavar='N',
-                        help='batch size')
-    parser.add_argument('--target-seq-len', type=int, default=35,
-                        help='sequence length')
-
-    parser.add_argument('--lr', type=float, default=20,
-                        help='initial learning rate')
-    parser.add_argument('--beta', type=float, default=0,
-                        help='L2 regularization penalty')
-    parser.add_argument('--clip', type=float, default=0.25,
-                        help='gradient clipping')
-    parser.add_argument('--epochs', type=int, default=40,
-                        help='upper epoch limit')
-
-    parser.add_argument('--seed', type=int, default=1111,
-                        help='random seed')
-    parser.add_argument('--cuda', action='store_true',
-                        help='use CUDA')
-    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
-                        help='report interval')
-    parser.add_argument('--val-interval', type=int, default=1000000, metavar='N',
-                        help='validation interval in number of tokens')
-    parser.add_argument('--workdir',
-                        help='where to put models, logs etc.')
-    parser.add_argument('--load', type=str, required=True,
-                        help='where to load a model from')
-    parser.add_argument('--save', type=str, required=True,
-                        help='path to save the final model')
-    args = parser.parse_args()
+def main(args):
     print(args)
 
     init_seeds(args.seed, args.cuda)
@@ -138,4 +100,48 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.INFO, format='[%(levelname)s::%(name)s] %(message)s')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', type=str, required=True,
+                        help='location of the train corpus')
+    parser.add_argument('--valid', type=str, required=True,
+                        help='location of the valid corpus')
+    parser.add_argument('--characters', action='store_true',
+                        help='work on character level, whitespace is significant')
+    parser.add_argument('--shuffle-lines', action='store_true',
+                        help='shuffle lines before every epoch')
+
+    parser.add_argument('--batch-size', type=int, default=20, metavar='N',
+                        help='batch size')
+    parser.add_argument('--target-seq-len', type=int, default=35,
+                        help='sequence length')
+
+    parser.add_argument('--lr', type=float, default=20,
+                        help='initial learning rate')
+    parser.add_argument('--beta', type=float, default=0,
+                        help='L2 regularization penalty')
+    parser.add_argument('--clip', type=float, default=0.25,
+                        help='gradient clipping')
+    parser.add_argument('--epochs', type=int, default=40,
+                        help='upper epoch limit')
+
+    parser.add_argument('--seed', type=int, default=1111,
+                        help='random seed')
+    parser.add_argument('--cuda', action='store_true',
+                        help='use CUDA')
+    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                        help='report interval')
+    parser.add_argument('--val-interval', type=int, default=1000000, metavar='N',
+                        help='validation interval in number of tokens')
+    parser.add_argument('--workdir',
+                        help='where to put models, logs etc.')
+    parser.add_argument('--load', type=str, required=True,
+                        help='where to load a model from')
+    parser.add_argument('--save', type=str, required=True,
+                        help='path to save the final model')
+    args = parser.parse_args()
+
+    if args.cuda:
+        gpu_owner = GPUOwner()
+
+    main(args)
