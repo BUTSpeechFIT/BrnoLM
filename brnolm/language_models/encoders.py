fcf0ff7114aaa32b3160f98443b54041fd5d9059
Benes Karel <ibenes@fit.vutbr.cz>
2020-12-09 13:22:00 +0100

    Nicen the initialization of embeddings

--- brnolm/language_models/encoders.py
+++ brnolm/language_models/encoders.py
@@ -2,12 +2,10 @@ import torch.nn as nn
 
 
 class FlatEmbedding(nn.Module):
-    def __init__(self, nb_tokens, dim_embs):
+    def __init__(self, nb_tokens, dim_embs, init_range=0.1):
         super().__init__()
         self.embeddings = nn.Embedding(nb_tokens, dim_embs)
-
-        initrange = 0.1
-        self.embeddings.weight.data.uniform_(-initrange, initrange)
+        nn.init.uniform_(self.embeddings.weight, -init_range, init_range)
 
     def forward(self, x):
         return self.embeddings(x)
